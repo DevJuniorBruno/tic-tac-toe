4d1043f438014db1c8d305cc866a68da5f3b8f9e
xenorbil <brunode2001@gmail.com>
2022-07-18 14:08:46 -0300

    att version 1.0

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const player1 = "X";
-const player2 = "O";
+const player1 = prompt("NOME DO PLAYER X:");
+const player2 = prompt("NOME DO PLAYER O:");
 var playTime = player1;
 var fimDeJogo = false;
+var player = document.querySelectorAll("div#contador img") [0];
+var posicao = document.getElementsByClassName("posição");
+var vencedor = '';
 
 mostrador();
 initialize();
@@ -11,18 +14,13 @@ function mostrador() {
 
     if(fimDeJogo) {return;}
     if(playTime == player1) {
-        var player = document.querySelectorAll("div#contador img") [0];
         player.setAttribute("src", "img/x.png");
-
     }else {
-        var player = document.querySelectorAll("div#contador img") [0];
         player.setAttribute("src", "img/circulo.png")
     }
 }
 
 function initialize(){
-
-    var posicao = document.getElementsByClassName("posição");
 
     for(var cont = 0; cont < posicao.length;cont++) {
 
@@ -41,15 +39,60 @@ function initialize(){
                 this.innerHTML = "<img src = 'img/circulo.png'>";
                 this.setAttribute('jogada', player2);
                 playTime= player1;
-            }   
+            }
+
             mostrador()
-            initialize()
-            }  
+            resultado()
+
+            }
+
         })
 
-        
-
     }
-
-
 } 
+
+ function resultado() {
+
+    var x1 = document.getElementById('x1').getAttribute("jogada");
+    var x2 = document.getElementById('x2').getAttribute("jogada");
+    var x3 = document.getElementById('x3').getAttribute("jogada");
+
+    var y1 = document.getElementById('y1').getAttribute("jogada");
+    var y2 = document.getElementById('y2').getAttribute("jogada");
+    var y3 = document.getElementById('y3').getAttribute("jogada");
+
+
+    var z1 = document.getElementById('z1').getAttribute("jogada");
+    var z2 = document.getElementById('z2').getAttribute("jogada");
+    var z3 = document.getElementById('z3').getAttribute("jogada");
+
+    if( (x1 == x2 && x1 == x3 && x1 != "") || 
+        (x1 == y1 && x1 == z1 && x1 != "") || 
+        (x1 == y2 && x1 == z3 && x1 != "")) {
+            vencedor = x1;
+            console.log(vencedor);
+    }
+    if( (y2 == y1 && y2 == y3 && y2 != "") || 
+        (y2 == x2 && y2 == z2 && y2 != "") ||
+        (y2 == x3 && y2 == z1 && y2 != "")) {
+            vencedor = y2
+            console.log(vencedor);
+        }
+
+    if( (z3 == z2 && z3 == z1 && z3 != "") ||
+        (z3 == y3 && z3 == x3 && z3 != "") ){
+            vencedor = z3;
+            console.log(vencedor);
+        }
+
+        if(vencedor != ""){
+            fimDeJogo= true;
+                if(vencedor == player1)
+                    setTimeout(()=> { alert(`${player1} VENCEU O JOGO!!`)}, 500);
+                if(vencedor == player2)
+                    setTimeout (()=> {alert(`${player2} VENCEU O JOGO`)}, 500);
+            }
+
+ }
+ 
+
